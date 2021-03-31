b80a297003ccf43fd42df567ea2498c1eb086cdb
unknown <ibmjavanh17@iiht.tech>
2021-03-31 12:48:36 +0530

    updated bug class

--- bug-service/src/main/java/com/ibm/bug/Bug.java
+++ bug-service/src/main/java/com/ibm/bug/Bug.java
@@ -1,16 +1,137 @@
 package com.ibm.bug;
 
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+
 public class Bug {
+	@Id
+	private String id;
+	@NotNull
+	@NotBlank
 	private String name;
+	@NotNull
+	@NotBlank
 	private String priority;
 	private String type;
+	@NotNull
+	@NotBlank
 	private String status;
+	@NotNull
+	@NotBlank
 	private String module;
 	private String buildVersion;
 	private String severity;
+	@NotNull
+	@NotBlank
 	private String projectId;
 	private String developerId;
+	@NotNull
+	@NotBlank
 	private String testerId;
+	@NotNull
+	@NotBlank
 	private String product;
+	@NotNull
+	@NotBlank
 	private String synopsis;
+	@NotNull
+	@NotBlank
+	private String description;
+	private Date submittedOn;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getModule() {
+		return module;
+	}
+	public void setModule(String module) {
+		this.module = module;
+	}
+	public String getBuildVersion() {
+		return buildVersion;
+	}
+	public void setBuildVersion(String buildVersion) {
+		this.buildVersion = buildVersion;
+	}
+	public String getSeverity() {
+		return severity;
+	}
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+	public String getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+	public String getDeveloperId() {
+		return developerId;
+	}
+	public void setDeveloperId(String developerId) {
+		this.developerId = developerId;
+	}
+	public String getTesterId() {
+		return testerId;
+	}
+	public void setTesterId(String testerId) {
+		this.testerId = testerId;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public String getSynopsis() {
+		return synopsis;
+	}
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getSubmittedOn() {
+		return submittedOn;
+	}
+	public void setSubmittedOn(Date submittedOn) {
+		this.submittedOn = submittedOn;
+	}
+	
 }
