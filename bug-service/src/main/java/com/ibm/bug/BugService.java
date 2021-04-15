27327a193e5af2a1ca98780380b08b5544884ac1
unknown <ibmjavanh17@iiht.tech>
2021-04-15 17:23:51 +0530

    added bug status validation

--- bug-service/src/main/java/com/ibm/bug/BugService.java
+++ bug-service/src/main/java/com/ibm/bug/BugService.java
@@ -26,7 +26,59 @@ public class BugService {
 	}
 
 	public void updateBug(Bug bug) {
-		bugRepository.save(bug);
+		int valid=0;
+		String bugId = bug.getId();
+		STATUS newStatus = bug.getStatus();
+		STATUS oldStatus = STATUS.NEW;
+		List<Bug> bugs = bugRepository.findAll();
+		for (Bug eachBug : bugs) {
+			if (bugId.equals(eachBug.getId())) {
+				oldStatus = eachBug.getStatus();
+			}
+		}
+		if (newStatus.equals(STATUS.ASSIGNED) && (oldStatus.equals(STATUS.NEW) || oldStatus.equals(STATUS.REOPEN) || 
+				oldStatus.equals(STATUS.DEFERRED))) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.ASSIGNED) && (newStatus.equals(STATUS.OPEN) || newStatus.equals(STATUS.DEFERRED)
+				|| newStatus.equals(STATUS.DUPLICATE) || newStatus.equals(STATUS.REJECTED)
+				|| newStatus.equals(STATUS.NOT_A_BUG))) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.OPEN) && (newStatus.equals(STATUS.FIXED) || newStatus.equals(STATUS.NEED_MORE_INFO)
+						|| newStatus.equals(STATUS.COULDNOT_REPRODUCE) || newStatus.equals(STATUS.WONT_FIX))) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.FIXED) && newStatus.equals(STATUS.PENDING_RETEST)) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.PENDING_RETEST) && newStatus.equals(STATUS.RETEST)) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.RETEST) && (newStatus.equals(STATUS.VERIFIED) || newStatus.equals(STATUS.REOPEN))) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.VERIFIED) && newStatus.equals(STATUS.CLOSED)) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.NEED_MORE_INFO) && newStatus.equals(STATUS.DEFERRED)) {
+			valid=1;
+		}
+		else if (newStatus.equals(STATUS.CLOSED) && (oldStatus.equals(STATUS.COULDNOT_REPRODUCE) || oldStatus.equals(STATUS.WONT_FIX)
+				|| oldStatus.equals(STATUS.DUPLICATE) || oldStatus.equals(STATUS.REJECTED)
+				|| oldStatus.equals(STATUS.NOT_A_BUG))) {
+			valid=1;
+		}
+		else if (oldStatus.equals(STATUS.CLOSED) && newStatus.equals(STATUS.CLOSED)) {
+			valid=1;
+		}
+		if(valid==1) {
+			bugRepository.save(bug);
+		}
+		else {
+			throw new IllegalArgumentException("Invalid status");
+		}
+		
 
 	}
 
